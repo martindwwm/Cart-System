ba0b5d0f270119b3846287d5bbeb82d8fd5cb828
martindwwm <kirizvki0@gmail.com>
2025-01-21 00:52:53 +0100

    add to cart

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,33 @@
-export let cart = JSON.parse(localStorage.getItem("cart")) || [
-  {
-    id: "MG-FIG-001-202300001",
-    quantity: 1,
-  },
-  {
-    id: "MG-FIG-002-602392074",
-    quantity: 2,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+export function addToCart(productId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  const quantitySelector = document.querySelector(
+    `.js-quantity-selector-${productId}`
+  );
+  const quantity = Number(quantitySelector.value);
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId,
+      quantity,
+    });
+  }
+}
+
+export function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
