6e3b419f37ee4bfbfdc0173cfe91d48d82029950
martindwwm <kirizvki0@gmail.com>
2025-03-11 00:13:50 +0100

    Create oop version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,104 @@
+function Cart(localStorageKey) {
+  const cart = {
+    cartItems: JSON.parse(localStorage.getItem(localStorageKey)) || [
+      {
+        productId: "MG-FIG-001-202300001",
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "MG-FIG-002-602392074",
+        quantity: 1,
+        deliveryOptionId: "2",
+      },
+    ],
+
+    saveToStorage() {
+      localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
+    },
+
+    addToCart(productId) {
+      let matchingItem;
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+      // const quantity = Number(quantitySelector.value);
+
+      if (matchingItem) {
+        matchingItem.quantity++;
+      } else {
+        this.cartItems.push({
+          productId,
+          quantity: 1,
+          deliveryOptionId: "1",
+        });
+      }
+
+      this.saveToStorage();
+    },
+
+    removeFromCart(productId) {
+      const newCart = [];
+      this.cartItems.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+          newCart.push(cartItem);
+        }
+      });
+
+      this.cartItems = newCart;
+
+      this.saveToStorage();
+    },
+
+    calculateQuantity() {
+      let cartQuantity = 0;
+      this.cartItems.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+      });
+
+      return cartQuantity;
+    },
+
+    updateQuantity(productId, newQuantity) {
+      let matchingItem;
+
+      this.cartItems.forEach((cartItem) => {
+        if (cartItem.productId === productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      matchingItem.quantity = newQuantity;
+
+      this.saveToStorage();
+    },
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+      let matchingItem;
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      matchingItem.deliveryOptionId = deliveryOptionId;
+
+      this.saveToStorage();
+    },
+  };
+
+  return cart;
+}
+
+const cart = Cart("cart-oop");
+const businessCart = Cart("cart-business");
+
+businessCart.addToCart("MG-FIG-003-001372053");
+console.log(cart);
+console.log(businessCart);
